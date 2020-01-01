b1a6d375c3aaed84e7826bca91f1617d11a74888
Tianshu Bao <tianshubao11@gmail.com>
2020-01-01 15:44:09 -0600

    Update DiscretePDBaseComponent.java

--- src/java/com/verivital/hyst/ir/base/DiscretePDBaseComponent.java
+++ src/java/com/verivital/hyst/ir/base/DiscretePDBaseComponent.java
@@ -368,4 +368,26 @@ public class DiscretePDBaseComponent extends Component
 		return rv;
 	}
 	
+	public DiscretePDBaseComponent discretize(int sPoint, int ePoint, int numOfMeshpoints, int[] initCondition)// must need an initial condition
+	{
+		DiscretePDBaseComponent dspdha = new DiscretePDBaseComponent(numOfMeshpoints);
+		String[] discretePartition = new String[numOfMeshpoints];	//Discrete partition is decided by the number of mesh points.
+		String[] modeName = modes.KeySet().toArray();	//names of all modes
+		int numOfModes = modes.size();
+		for(int k = 0; k < numOfMeshpoints; k++){
+			discretePartition[k] = modeName[0];			
+		}		
+		
+		//consider permutation next, cant use 2 forloops
+		for(int i = 0; i < numOfMeshpoints; i++){
+			for(int j = 0; j < numOfModes; j++){
+				discretePartition[i] = modeName[j];
+				dspdha.createMode(discretePartition);				
+			}
+		}
+		
+
+		return dspdha;
+	}	
+	
 }
