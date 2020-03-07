6ee517437c4b68834ae09824d2ab17ad63b724fc
Tianshu Bao <tianshubao11@gmail.com>
2020-03-06 22:28:56 -0600

    Update PDHAPrinter.java

--- src/java/com/verivital/hyst/printers/PDHAPrinter.java
+++ src/java/com/verivital/hyst/printers/PDHAPrinter.java
@@ -54,25 +54,16 @@ public class PDHAPrinter extends ToolPrinter
 	// command line parser for tools
 	private CmdLineParser parser = new CmdLineParser(this);
 
-	// printing
-	public enum OutputType
+
+
+
+
+	@Override
+	protected String getCommentPrefix()
 	{
-		STDOUT, GUI, FILE, NONE, STRING,
-	};
-
-	public ToolPrinter()
-	{
-		String flag = getCommandLineFlag();
-
-		if (flag.startsWith("-"))
-			throw new RuntimeException(
-					"tool printer's command-line flag shouldn't start with a hyphen: " + flag);
-
-		// skip the affine transformation conversion by default
-		preconditions.skip(PreconditionsFlag.CONVERT_AFFINE_TERMS);
+		return "%";
 	}
-
-
+	
 	protected void printDocument(String originalFilename)
 	{
 		TreeMap<String, String> mapping = new TreeMap<String, String>();
@@ -98,23 +89,150 @@ public class PDHAPrinter extends ToolPrinter
 	 */
 	private void printProcedure()
 	{
-		printmodule();
-
 		printVars();
 
 		// printSettings(); // TODO: print to other file or command line call
+		
+		printInitialConditions();
+		
+		printInput();
 
-		printConstants();
+		printComputing();
 
-		printModes();
+		printPlotting();
 
-		printJumps();
-
-		printInitialStates();
-
-		printGoalStates();
 	}
 	
+	
+	private void printVars()
+	{
+		printLine("function heater_model");
+
+
+		printComment("Locations are encoded as a variable with a finite (enumeration / set) type.");
+		this.increaseIndentation();
+
+
+		printLine("h = 0.5");
+		printLine("delta = 0.5");
+		printLine("m = 10/h + 1;" + commentChar + "number of mesh points");
+		printLine("t = 10/deltat + 1;" + commentChar + "number of time steps");
+		printLine("alpha = deltat/(h*h);");
+		
+		printLine("A = zeros(m - 2, m - 2);");
+			
+		
+		this.decreaseIndentation();
+		
+	}	
+
+	private void printInitialConditions()
+	{
+		printNewline();		
+		this.increaseIndentation();
+		
+		printComment("Intial condition");
+		printLine("u0 = ones(m - 2, 1);");
+		printLine("u0 = u0 * 0.2;");
+																				// figure
+																				// out
+		this.decreaseIndentation();
+	}
+	
+	private void printInput()
+	{
+		printNewline();		
+		this.increaseIndentation();
+		
+		printComment("Input function");		
+		printLine("function f = input_fun(x)");	
+		printLine(indentation + "f = -0.1*x + 1;");
+		printLine("end");
+		
+		printNewline();			
+		printLine("load = zeros(m - 2, 1);");
+		printLine("u = zeros(m - 2, t);");
+		printLine("u(:,1) = u0;");
+		
+		this.decreaseIndentation();	
+	};
+	
+	
+	private void printComputing()
+	{
+		printNewline();	
+		this.increaseIndentation();	
+
+			printLine("for i = 2 : t");
+			this.increaseIndentation();
+
+				printLine("for j = 1 : m - 2");	
+
+				this.increaseIndentation();
+
+					printLine("if i == 2");
+					printLine(indentationAmount + "load(j, 1) = deltat * input_fun(h * j);");
+					printLine("end");
+
+					printLine("if load(j, 1) == 0");
+					printLine(indentationAmount + "load(j, 1) = 0;");
+					printLine("else");
+					printLine(indentationAmount + "load(j, 1) = deltat * input_fun(h * j);");
+					printLine("end");
+
+					printComment("switching part");	
+
+					printLine("if u(j, i - 1) > 0.7");
+					printLine(indentationAmount + "load(j, 1) = 0;");
+					printLine("elseif u(j, i - 1) < 0.4");
+					printLine(indentationAmount + "load(j, 1) = deltat * input_fun(h * j);");
+					printLine("end");
+
+				this.decreaseIndentation();
+				printLine("end");
+
+			printLine("u(:, i) = A * u(:, i - 1) + load;");	
+			this.decreaseIndentation();
+			printLine("end");
+
+		this.decreaseIndentation();
+	
+	};	
+	
+	private void printPlotting()
+	{
+
+		printNewline();		
+		this.increaseIndentation();
+		
+		printComment("Plotting");
+		printLine("xlist = linspace(0,10,m);");	
+		printLine("tlist = linspace(0,10,t);");
+		printLine("u = [zeros(1, t); u; zeros(1, t)];");
+		printLine("u = u';");
+		
+		printLine("surf(xlist,tlist,u)");	
+		printLine("title('Numerical solution computed with 100 mesh points.')");
+		printLine("xlabel('Distance x')");
+		printLine("ylabel('Time t')");		
+
+		printLine("figure");	
+		printLine("plot(xlist,u(91,:))");
+		printLine("title('Solution at t = 9')");
+		printLine("xlabel('Distance x')");	
+		printLine("ylabel('u(x,5)')");
+		
+		printLine("figure");	
+		printLine("plot(tlist,u(:,5))");
+		printLine("title('Solution at x = 4')");
+		printLine("xlabel('Time t')");	
+		printLine("ylabel('u(0,t)')");	
+
+		
+		this.decreaseIndentation();
+	
+	};
+
 	
 	public void setConfig(Configuration c)
 	{
