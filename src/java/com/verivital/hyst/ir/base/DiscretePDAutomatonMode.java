d744f6a87d6c0aabee89b1fabed525087e1bafab
Tianshu Bao <tianshubao11@gmail.com>
2020-04-06 20:26:10 -0500

    Update DiscretePDAutomatonMode.java

--- src/java/com/verivital/hyst/ir/base/DiscretePDAutomatonMode.java
+++ src/java/com/verivital/hyst/ir/base/DiscretePDAutomatonMode.java
@@ -29,7 +29,7 @@ import com.verivital.hyst.util.AutomatonUtil;
  */
 public class DiscretePDAutomatonMode extends AutomatonMode
 {
-	public List<String> names;
+	public ArrayList<String> names;
 	public final DiscretePDBaseComponent automaton;
 	public Expression invariant;
 	public boolean urgent = false;
